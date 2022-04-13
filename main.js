40e2f2b8d402096ecfbde45bc6853837da91eba3
emidi785 <103552241+emidi785@users.noreply.github.com>
2022-04-13 00:55:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let btnMenu = document.querySelector('.btn');
+let menu = document.querySelector('.nav__ul');
+let line1 = document.getElementById('spn1');
+let line2 = document.getElementById('spn2');
+let line3 = document.getElementById('spn3');
+
+btnMenu.addEventListener('click', () =>{
+    menu.classList.toggle('menu-visible');
+    line1.classList.toggle('spn1');
+    line2.classList.toggle('spn2');
+    line3.classList.toggle('spn3');
+});
